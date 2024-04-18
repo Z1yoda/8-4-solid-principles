c11b9895fdc68d44f6ecf1157c8fd9ea449da22f
Z1yoda <ziyoda01092007@gmail.comgit init git config  --global user.email ziyoda01092007@gmil.com>
2024-04-18 10:24:20 +0500

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>SOLID Principles</h1>
+      <div style={{ width: "800px", height: "700px" }}>
+        <div style={{ position: "relative", paddingTop: "56.25%", width: "100%" }}><iframe src="https://kinescope.io/embed/cf2k9YYeNfXeuFDgSoHYez" allow="autoplay; fullscreen; picture-in-picture; encrypted-media; gyroscope; accelerometer; clipboard-write;" frameBorder="0" allowFullScreen style={{ position: "absolute", width: "100%", height: "100%", top: "0", left: "0" }}></iframe></div>
+      </div>
+    </>
+  )
+}
+
+export default App
